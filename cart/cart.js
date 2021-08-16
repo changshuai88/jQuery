4ec7d6d43dc9164f7f5cffa08aabf892874cadf2
2233282817 <2233282817@qq.com>
2021-08-16 13:20:44 +0800

    下面的全选按钮的优化

--- cart/cart.js
+++ cart/cart.js
@@ -3,7 +3,7 @@ $(function(){
     //  就是把全选按钮(checkall)的状态赋值给 三个小的按钮(j-checkbox)就可以了
     //事件可以使用change
     $(".checkall").change(function(){
-         $(".j-checkbox,.checkall").prop("checked",$(".checkall").prop("checked"));
+         $(".j-checkbox,.checkall").prop("checked",$(this).prop("checked"));
     })
 
     // 2.如果小复选框被选中的个数等于3 就应该把全选按钮选上，否则全选按钮不选
