4189d3fb0baf36a8528df72c28cedbfc72b35c31
2233282817@qq.com <2233282817@qq.com>
2021-09-05 14:13:27 +0800

    todolist 渲染到页面

--- todolist/js/todolist.js
+++ todolist/js/todolist.js
@@ -2,19 +2,23 @@ $(function(){
     // alert(11);
     // 1.按下回车把完整数据存储到本地存储里面
     //存储的数据格式 var todolist =[{title:"xxx"，done:false}]
+    load();
     $("#title").on("keydown",function(event){
         if(event.keyCode === 13){
             // console.log(event);
             // alert(11);
             //先读取本地存储原来的数据。
             var local=getData();
-            console.log(local);
+            // console.log(local);
 
             //把local数组进行更新数据，把最新的数据追加给local数组
             local.push({title: $(this).val() , done:false});
 
             //把这个数组local存储到本地存储里面
             saveData(local);
+            
+            // 2.todolist本地存储数据渲染加载到页面
+            load();
         }
     })
 
@@ -32,5 +36,19 @@ $(function(){
     //保存本地存储数据
     function saveData(data){
         localStorage.setItem("todolist",JSON.stringify(data));
+    }
+
+    //渲染加载数据
+    function load(){
+        //读取本地存储的数据
+        var data = getData();
+        // console.log(data);
+        //遍历之前先清空ol里面的元素内容
+        $("ol").empty();
+        //遍历这个数据
+        $.each(data,function(i,n){
+            // console.log(n);
+            $("ol").prepend("<li><input type='checkbox' ><p>"+n.title+"</p> <a href='javascript:;'></a></li>");
+        })
     }
 })
